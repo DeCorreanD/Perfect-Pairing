9491381137902f7aed3e952751245e7b21b4d7fc
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2023-02-28 19:21:42 -0600

    Made API calls to be able to get CRUD on the SitterData

---
+++ api/sitterdata.js
@@ -0,0 +1,78 @@
+import { clientCredentials } from '../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getSitters = () => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/sitters.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        resolve(Object.values(data));
+      } else {
+        resolve([]);
+      }
+    })
+    .catch(reject);
+});
+
+const getSingleSitter = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/sitters/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createSitter = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/sitters.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateSitter = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/sitters/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const deleteSitter = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/sitters/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+export {
+  deleteSitter,
+  getSingleSitter,
+  getSitters,
+  createSitter,
+  updateSitter,
+};
