d4df6555eec587c5e96c8546b5a209331bee6fee
DeCorrean Davis <112592380+DeCorreanD@users.noreply.github.com>
2023-02-28 21:12:34 -0600

    .

---
+++ pages/users/edit/[firebaseKey].js
@@ -0,0 +1,17 @@
+// import React, { useEffect, useState } from 'react';
+// import { useRouter } from 'next/router';
+// import { getSingleUser } from '../api/usersData';
+
+// export default function EditUser() {
+//   const [editItem, setEditItem] = useState({});
+//   const router = useRouter();
+//   // Grab the firebaseKey
+//   const { firebaseKey } = router.query;
+
+//   // Make a call to the API to get the User Data
+//   useEffect(() => {
+//     getSingleUser(firebaseKey).then(setEditItem);
+//   }, [firebaseKey]);
+
+//   // Pass object to Form
+// }
